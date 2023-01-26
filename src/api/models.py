922e6b365636a397cd1a8f884984ff74b9182fa3
ipeters123 <isaiah.peters64@gmail.com>
2023-01-26 20:16:43 +0000

    JWT Authentication

--- src/api/models.py
+++ src/api/models.py
@@ -4,12 +4,13 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True, default=True)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User {self.id}>'
 
     def serialize(self):
         return {
@@ -17,3 +18,33 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Dream(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    meaning = db.Column(db.String(200), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Dream %r>' % self.meaning
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "meaning": self.meaning,
+            # do not serialize the password, its a security breach
+        }     
+
+class Nightmare(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    meaning = db.Column(db.String(200), unique=True, nullable=False)
+    
+    def __repr__(self):
+        return '<Nightmare %r>' % self.meaning
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "meaning": self.meaning,
+            # do not serialize the password, its a security breach
+        }     
--- src/app.py
+++ src/app.py
@@ -10,6 +10,7 @@ from api.utils import APIException, generate_sitemap
 from api.models import db
 from api.routes import api
 from api.admin import setup_admin
+from flask_jwt_extended import JWTManager
 from api.commands import setup_commands
 
 #from models import Person
@@ -18,6 +19,10 @@ ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] =os.environ.get('JWT_SECRET')  # Change this!
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -50,19 +55,46 @@ def handle_invalid_usage(error):
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
-    if ENV == "development":
         return generate_sitemap(app)
-    return send_from_directory(static_file_dir, 'index.html')
 
-# any other endpoint will try to serve it like a static file
-@app.route('/<path:path>', methods=['GET'])
-def serve_any_other_file(path):
-    if not os.path.isfile(os.path.join(static_file_dir, path)):
-        path = 'index.html'
-    response = send_from_directory(static_file_dir, path)
-    response.cache_control.max_age = 0 # avoid cache memory
-    return response
 
+@app.route('/user', methods=['GET'])
+def handle_hello():
+
+    users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), users))
+    
+    return jsonify(response_body), 200
+
+@app.route('/user', methods=['POST'])
+def create_user():
+
+    request_body_user = request.get_json()
+    
+    earth = User(first_name=request_body_user["first_name"], email=request_body_user["email"], password=request_body_user["password"])
+    db.session.add(earth)
+    db.session.commit()
+
+    return jsonify(request_body_user), 200
+
+@app.route('/user/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+
+    request_body_user = request.get_json()
+
+    user1 = User.query.get(user_id)
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+
+    if "username" in request_body_user:
+        user1.username = body["username"]
+    if "email" in request_body_user:
+        user1.email = body["email"]
+    if "first_name" in request_body_user:
+        user1.first_name = request_body_user["email"]
+    db.session.commit()
+    
+    return jsonify(request_body_user), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
